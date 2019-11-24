ed3d250d3b1597098b6d26a0046036fc66d5b0a3
manbow-42 <yuukisasaki42@icloud.com>
2019-11-24 17:58:01 +0900

    ツイート周り

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,45 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(1)
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if current_user.id == tweet.user_id
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if current_user.id == tweet.user_id
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+    end
+
+    def move_to_index
+      redirect_to root_path unless user_signed_in?
+    end
+end
