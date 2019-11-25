a8135fdb4dfbc5c4840eb2d127310049c8c35113
manbow-42 <yuukisasaki42@icloud.com>
2019-11-25 09:20:31 +0900

    ログアウト後のリダイレクト先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_paremeters, if: :devise_controller?
   
   def configure_permitted_paremeters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
   end
 end
