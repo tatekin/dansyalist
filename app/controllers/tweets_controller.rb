498ec7b8e46f245a31711b039262d5e972ad12a9
manbow-42 <yuukisasaki42@icloud.com>
2019-12-02 08:38:09 +0900

    update tweets_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,13 @@
 class TweetsController < ApplicationController
-
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(1)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(3)
   end
 
   def show
     @tweet = Tweet.find(params[:id])
-    @comment = Comment.new
+    @comment = @tweet.comments.build
     @comments = @tweet.comments.includes(:user)
   end
 
@@ -17,7 +16,12 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -26,20 +30,21 @@ class TweetsController < ApplicationController
 
   def update
     tweet = Tweet.find(params[:id])
-    tweet.update(tweet_params) if current_user.id == tweet.user_id
+    if tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
   
   def destroy
     tweet = Tweet.find(params[:id])
-    tweet.destroy if current_user.id == tweet.user_id
+    tweet.destroy
+    redirect_to root_path
   end
 
   private
     def tweet_params
       params.require(:tweet).permit(:text, images: []).merge(user_id: current_user.id)
-    end
-
-    def move_to_index
-      redirect_to new_user_session_path unless user_signed_in?
     end
 end
