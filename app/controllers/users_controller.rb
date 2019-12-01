6c6b24fbae7df62d9281229b75e2736dc7521d67
manbow-42 <yuukisasaki42@icloud.com>
2019-12-02 08:38:21 +0900

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:following, :followers]
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(1).order("created_at DESC")
+    @user = User.find(params[:id])
+    @tweets = @user.tweets.order("created_at DESC").page(params[:page]).per(3)
+  end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following.page(params[:page]).per(3)
+    # render "show_follow"
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.page(params[:page]).per(3)
+    # render "show_follow"
   end
 end
