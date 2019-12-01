ef4c9310c17f0a9916435e32ac70b0956e80e626
manbow-42 <yuukisasaki42@icloud.com>
2019-12-02 08:37:21 +0900

    create comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
+def create
+  tweet = Tweet.find(params[:tweet_id])
+  @comment = tweet.comments.build(comment_params)
+  if @comment.save
+    redirect_back(fallback_location: tweet_path(tweet))
+  else
+    flash[:danger] = "入力してください"
+    redirect_back(fallback_location: tweet_path(tweet))
+  end
+end
 
-  def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet_id}"
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    @comment = tweet.comments.find(params[:id])
+    @comment.destroy
+    redirect_back(fallback_location: tweet_path(tweet))
   end
 
   private
     def comment_params
-      params.permit(:text, :tweet_id).merge(user_id: current_user.id)
+      params.require(:comment).permit(:text).merge(user_id: current_user.id)
     end
 end
