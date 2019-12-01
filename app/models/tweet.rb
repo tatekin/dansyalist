6bf747aafbba6546bec632d6e990d3426cfa26e2
manbow-42 <yuukisasaki42@icloud.com>
2019-12-02 08:39:43 +0900

    update tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many_attached :images
 
   validates :text, presence: true
