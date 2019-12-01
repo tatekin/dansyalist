b56d5625488427f95a9a4645bc173b134c46a5f9
manbow-42 <yuukisasaki42@icloud.com>
2019-12-02 08:45:39 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   resources :tweets do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 end
